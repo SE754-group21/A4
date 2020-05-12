840a671d8248108b745f93b211097868db148a66
Lola <lola.c.garden@gmail.com>
2020-05-13 01:18:17 +1200

    testcase: testGetCourseEnrollmentStatusInvalidCourse

--- src/test/java/testCourseEnrollmentStatus.java
+++ src/test/java/testCourseEnrollmentStatus.java
@@ -2,6 +2,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.NoSuchElementException;
+
 public class testCourseEnrollmentStatus {
 
     @Test
@@ -22,6 +24,24 @@ public class testCourseEnrollmentStatus {
         EnrollmentStatusEnum status = enrollmentHandler.getEnrollmentStatusForCourse(sid, cid);
 
         Assert.assertEquals(EnrollmentStatusEnum.enrolled, status);
+    }
+
+
+    @Test (expected = NoSuchElementException.class)
+    public void testGetCourseEnrollmentStatusInvalidCourse() {
+
+        Course course = Mockito.mock(Course.class);
+        Student student = Mockito.mock(Student.class);
+        Mockito.when(student.getEnrollmentStatusForCourse(course)).thenReturn(null);
+
+        String sid = "A123";
+        Mockito.when(student.getSid()).thenReturn(sid);
+        String cid = "SE867";
+        Mockito.when(course.getCid()).thenReturn(cid);
+
+        EnrollmentHandler enrollmentHandler = new EnrollmentHandler();
+        enrollmentHandler.addStudent(student);
+        EnrollmentStatusEnum status = enrollmentHandler.getEnrollmentStatusForCourse(sid, cid);
     }
 
 
