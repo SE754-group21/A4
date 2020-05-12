192250f17ef59aeb8b18ff82988ad39856f54a5a
Tia Saunders <tsau354@aucklanduni.ac.nz>
2020-05-12 19:14:29 +1200

    testcase: testGetCompulsoryCourses

--- src/test/java/testDegreeInformation.java
+++ src/test/java/testDegreeInformation.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.junit.Assert.assertEquals;
@@ -29,6 +31,26 @@ public class testDegreeInformation {
         Mockito.when(degree.getDid()).thenReturn(did);
         Mockito.when(degree.getDname()).thenReturn(dname);
         String name = handler.getDname(did);
+    }
+
+    @Test
+    public void testGetCompulsoryCourses() {
+        String did = "SOFTENG";
+        String dname = "Software Engineering";
+        String cid = "ENGGEN303";
+
+        List<Course> compulsoryCourses = new ArrayList<Course>();
+        Course compulsoryCourse1 = Mockito.mock(Course.class);
+        Mockito.when(compulsoryCourse1.getCid()).thenReturn(cid);
+        compulsoryCourses.add(compulsoryCourse1);
+
+        DegreeHandler handler = new DegreeHandler();
+        Degree degree = Mockito.mock(Degree.class);
+        Mockito.when(degree.getDid()).thenReturn(did);
+        Mockito.when(degree.getCompulsoryCourses()).thenReturn(compulsoryCourses);
+        handler.addDegree(degree);
+        List<Course> courses = handler.getCompulsoryCourses(did);
+        assertEquals(dname, courses);
     }
 
 }
