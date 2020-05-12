d0fe35f32d82cb0d5da8f9f87b6549560f37cb19
Tia Saunders <tsau354@aucklanduni.ac.nz>
2020-05-12 18:45:41 +1200

    testcase: testGetDegreeNameInvalidID

--- src/test/java/testDegreeInformation.java
+++ src/test/java/testDegreeInformation.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.util.NoSuchElementException;
+
 import static org.junit.Assert.assertEquals;
 public class testDegreeInformation {
 
@@ -15,6 +18,17 @@ public class testDegreeInformation {
         String name = handler.getDname(did);
         assertEquals(dname, name);
     }
-    
+
+
+    @Test(expected = NoSuchElementException.class)
+    public void testGetDegreeNameInvalidID() {
+        String did = "SOFTENG";
+        String dname = "Software Engineering";
+        DegreeHandler handler = new DegreeHandler();
+        Degree degree = Mockito.mock(Degree.class);
+        Mockito.when(degree.getDid()).thenReturn(did);
+        Mockito.when(degree.getDname()).thenReturn(dname);
+        String name = handler.getDname(did);
+    }
 
 }
