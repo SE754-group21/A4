38683648889b2a49c376ebb30b5bed83dffa1ee0
Lola <lola.c.garden@gmail.com>
2020-05-13 01:17:40 +1200

    implementation: testGetEnrolledCourseEnrollmentStatus

--- src/main/java/EnrollmentHandler.java
+++ src/main/java/EnrollmentHandler.java
@@ -1,16 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class EnrollmentHandler {
 
+    private Map<String, Student> students ;
+    private Map<String, Course> courses;
+
+
     public EnrollmentHandler() {
+        students = new HashMap<String, Student>();
+        courses = new HashMap<String, Course>();
 
-    }
-
-    public void addStudent(Student student) {
     }
 
     public void addCourse(Course course) {
+        courses.put(course.getCid(), course);
     }
 
+    public void addStudent(Student student) {
+        students.put(student.getSid(), student);
+    }
+
+
     public EnrollmentStatusEnum getEnrollmentStatusForCourse(String sid, String cid) {
-        return null;
+        Student student = students.get(sid);
+        Course course = courses.get(cid);
+
+        return student.getEnrollmentStatusForCourse(course);
+
     }
 }
