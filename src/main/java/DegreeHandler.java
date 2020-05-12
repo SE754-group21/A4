ff091f4b12c1050424783ab32c6f8e35803d176c
Tia Saunders <tsau354@aucklanduni.ac.nz>
2020-05-12 18:41:35 +1200

    implementation: testGetDegreeName

--- src/main/java/DegreeHandler.java
+++ src/main/java/DegreeHandler.java
@@ -1,17 +1,18 @@
-
+import java.util.*;
 
 public class DegreeHandler {
+    private Map<String, Degree> degrees;
 
     public DegreeHandler() {
-
+        degrees = new HashMap<String, Degree>();
     }
 
     public void addDegree(Degree degree) {
-
+        degrees.put(degree.getDid(), degree);
     }
 
     public String getDname(String did){
-        return "";
+        return degrees.get(did).getDname();
     }
 
 }
