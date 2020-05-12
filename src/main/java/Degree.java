6e2d1bd2a422851ead1725e91ed1fb0609d2c173
Tia Saunders <tsau354@aucklanduni.ac.nz>
2020-05-12 17:27:15 +1200

    fixComplilationErrors:  testGetDegreeName

---
+++ src/main/java/Degree.java
@@ -0,0 +1,16 @@
+
+
+public class Degree {
+
+    public Degree(){
+
+    }
+
+    public String getDid(){
+        return "";
+    }
+
+    public String getDname(){
+        return "";
+    }
+}
---
+++ src/main/java/DegreeHandler.java
@@ -0,0 +1,17 @@
+
+
+public class DegreeHandler {
+
+    public DegreeHandler() {
+
+    }
+
+    public void addDegree(Degree degree) {
+
+    }
+
+    public String getDname(String did){
+        return "";
+    }
+
+}
