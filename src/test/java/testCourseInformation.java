449e54fa713236356f7d71a071a1e956d37be06b
lucycy98 <lucy.chen.nz@gmail.com>
2020-05-12 15:19:10 +1200

    testcase: testGetStaff

--- src/test/java/testCourseInformation.java
+++ src/test/java/testCourseInformation.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 import static org.junit.Assert.assertEquals;
@@ -51,6 +53,19 @@ public class testCourseInformation {
         Mockito.when(course.getCid()).thenReturn(cid);
         Mockito.when(course.getCdesc()).thenReturn(cdesc);
         handler.getCdesc(cid);
+    }
+
+    @Test
+    public void testGetStaff() {
+        String cid = "SOFTENG754";
+        List<Staff> staff = new ArrayList<Staff>();
+        CourseHandler handler = new CourseHandler();
+        Course course = Mockito.mock(Course.class);
+        Mockito.when(course.getCid()).thenReturn(cid);
+        Mockito.when(course.getStaff()).thenReturn(staff);
+        handler.addCourse(course);
+        List<Staff> s = handler.getStaff(cid);
+        assertEquals(s.size(), 0);
     }
 
 
