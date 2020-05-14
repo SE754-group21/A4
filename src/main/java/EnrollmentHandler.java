2926e9f33636b12e430d819dec3e2cab1f4c30f7
lucycy98 <lucy.chen.nz@gmail.com>
2020-05-14 18:45:11 +1200

    implementation: testStudentNotMeetPrerequisitesInvalidSid

--- src/main/java/EnrollmentHandler.java
+++ src/main/java/EnrollmentHandler.java
@@ -24,6 +24,7 @@ public class EnrollmentHandler {
 
     public boolean studentMeetsPrerequisites(String sid, String cid) {
         if (courses.get(cid) == null) throw new NoSuchElementException("course with id not found");
+        if (students.get(sid) == null) throw new NoSuchElementException("student with id not found");
         Student student = students.get(sid);
         Course course = courses.get(cid);
         List<Course> studentTaken = student.getTakenCourses();
