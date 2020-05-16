f4e51b190bc6a4b80566a89059aaf96b4bcfac58
Tia Saunders <tsau354@aucklanduni.ac.nz>
2020-05-17 00:17:05 +1200

    fixCompilationErrors: testStudentPrereqsMetConcessionApplication

---
+++ src/main/java/ConcessionApplicationHandler.java
@@ -0,0 +1,15 @@
+
+
+
+public class ConcessionApplicationHandler {
+
+    private Database db;
+
+    public ConcessionApplicationHandler(Database db) {
+        this.db = db;
+    }
+
+    public String submitApplication(String sid, String cid, ConcessionApplication concessionApp) {
+        return null;
+    }
+}
