c963211e08f4356cfa3c4ec2f5ddbb9d0ca0ff5d
Tia Saunders <tsau354@aucklanduni.ac.nz>
2020-05-17 00:54:45 +1200

    testCase: testStudentPrereqsNotMetConcessionApplication

--- src/test/java/ConcessionApplicationTest.java
+++ src/test/java/ConcessionApplicationTest.java
@@ -45,4 +45,25 @@ public class ConcessionApplicationTest {
         assertEquals("Students may only submit one concession application for a course", concessionStatus);
     }
 
+    @Test
+    public void testStudentPrereqsNotMetConcessionApplication() {
+        String cid = "SE754";
+        String sid = "12345";
+        Database db = Mockito.mock(Database.class);
+
+        Student student = Mockito.mock(Student.class);
+        Course course = Mockito.mock(Course.class);
+        Mockito.when(db.getCourse(cid)).thenReturn(course);
+        Mockito.when(db.getStudent(sid)).thenReturn(student);
+        Mockito.when(student.getEnrollmentRequestStatusForCourse(course)).thenReturn(EnrollmentRequestStatusEnum.prerequisites_not_met);
+
+        ConcessionApplication concessionApp = Mockito.mock(ConcessionApplication.class);
+        Mockito.when(concessionApp.setConcessionStatus()).thenCallRealMethod();
+        ConcessionApplicationHandler handler =  new ConcessionApplicationHandler(db);
+
+        String concessionStatus = handler.submitApplication(sid, cid, concessionApp);
+
+        assertEquals("Concession application submitted", concessionStatus);
+    }
+
 }
