86f6544ea2ab7704825e91fcbcdb6c3522a19d07
isabellelucy <izzbee123@gmail.com>
2020-05-17 15:15:21 +1200

    testcase: testSuccessfulLogin

---
+++ src/test/java/SSOIntegrationTest.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class SSOIntegrationTest {
+    @Test
+    public void testSuccessfulLogIn(){
+        AuthenticateUser auth = new AuthenticateUser();
+        SSOMock sso = Mockito.mock(SSOMock.class);
+        String username = "ibea707";
+        String password = "password";
+
+        Mockito.when(sso.verify()).thenReturn(true);
+
+        String login = auth.verifyLogin(username, password);
+        assertEquals("Login successful", login);
+
+    }
+}
