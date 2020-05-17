90690a4fdc54277963d7f61921f80a2b1a3dc537
isabellelucy <izzbee123@gmail.com>
2020-05-17 15:35:14 +1200

    Revert "implementation: testSuccessfulLogin"
    This reverts commit fc70e00dd45604042eb2de12e3e5e54c0e084bb2.

--- src/main/java/AuthenticateUser.java
+++ src/main/java/AuthenticateUser.java
@@ -1,9 +1,5 @@
 public class AuthenticateUser {
     public String verifyLogin(String username, String password){
-        SSOMock sso = new SSOMock();
-        if (sso.verify(username, password) == true){
-            return "Login successful";
-        }
         return "";
     }
 }
