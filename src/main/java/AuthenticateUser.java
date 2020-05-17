fc70e00dd45604042eb2de12e3e5e54c0e084bb2
isabellelucy <izzbee123@gmail.com>
2020-05-17 15:22:54 +1200

    implementation: testSuccessfulLogin

--- src/main/java/AuthenticateUser.java
+++ src/main/java/AuthenticateUser.java
@@ -1,5 +1,9 @@
 public class AuthenticateUser {
     public String verifyLogin(String username, String password){
+        SSOMock sso = new SSOMock();
+        if (sso.verify(username, password) == true){
+            return "Login successful";
+        }
         return "";
     }
 }
