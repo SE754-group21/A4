16dd97825a110b60099d08c7298cb757ab289218
lucycy98 <lucy.chen.nz@gmail.com>
2020-05-19 17:56:16 +1200

    testcase: removeStudentWaitingList

--- src/test/java/WaitingListCourseTest.java
+++ src/test/java/WaitingListCourseTest.java
@@ -9,6 +9,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
@@ -31,7 +33,7 @@ public class WaitingListCourseTest {
         Student student = Mockito.mock(Student.class);
         course.addStudent(student);
         course.addStudent(student);
-        assert(enrolledList.size() == 1);
+        assertTrue(enrolledList.size() == 1);
     }
 
     @Test
@@ -39,7 +41,20 @@ public class WaitingListCourseTest {
         Student student = Mockito.mock(Student.class);
         course.addStudent(student);
         course.removeStudent(student);
-        assert(enrolledList.size() == 0);
+        assertTrue(enrolledList.size() == 0);
+    }
+
+    @Test
+    public void removeStudentWaitingList() {
+        Student student1 = Mockito.mock(Student.class);
+        Student student2 = Mockito.mock(Student.class);
+        course.setCapacity(1);
+        course.addStudent(student1);
+        course.addStudent(student2);
+        course.removeStudent(student1);
+        assertTrue(enrolledList.size() == 1);
+        assertTrue(waitingList.size() == 0);
+        assertFalse(enrolledList.contains(student1));
     }
 
 
