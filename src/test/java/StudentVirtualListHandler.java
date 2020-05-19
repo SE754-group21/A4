97179c98597d21704862f49407854e06db1caf5d
lucycy98 <lucy.chen.nz@gmail.com>
2020-05-19 22:58:23 +1200

    testcase: testcheckDenied

---
+++ src/test/java/StudentVirtualListHandler.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+public class StudentVirtualListHandler {
+    private Student student;
+
+    @Before
+    public void setUp(){
+        student = Mockito.spy(new Student());
+    }
+
+    @Test
+    public void testcheckDenied() {
+        Course course = Mockito.mock(Course.class);
+        Mockito.doReturn(ConcessionStatusEnum.pending).when(student).getConcessionStatus(course);
+        Mockito.doReturn(VirtualListEnum.waiting_list).when(student).getVirtualStatus(course);
+        student.updateVirtualList(course);
+        verify(student, never()).setEnrollmentStatusForCourse(Mockito.any(Course.class), Mockito.any(EnrollmentStatusEnum.class));
+    }
+}
