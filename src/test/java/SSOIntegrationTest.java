60032ef9abba43fd30f358922861080d0de4be34
isabellelucy <izzbee123@gmail.com>
2020-05-21 09:59:05 +1200

    refactoring SSOIntegration

--- src/test/java/SSOIntegrationTest.java
+++ src/test/java/SSOIntegrationTest.java
@@ -8,12 +8,18 @@ import static org.junit.Assert.*;
 
 @Category(UnitTests.class)
 public class SSOIntegrationTest {
+    private SSOMock sso;
+    private AuthenticateUser auth;
+    private String username = "ibea707";
+
+    @Before
+    public void setUp(){
+        sso = Mockito.mock(SSOMock.class);
+        auth = new AuthenticateUser(sso);
+    }
 
     @Test
     public void testSuccessfulLogIn(){
-        SSOMock sso = Mockito.mock(SSOMock.class);
-        AuthenticateUser auth = new AuthenticateUser(sso);
-        String username = "ibea707";
         String password = "password";
 
         Mockito.when(sso.verify(username, password)).thenReturn("Success");
@@ -25,9 +31,6 @@ public class SSOIntegrationTest {
 
     @Test
     public void testUnsuccessfulLogIn(){
-        SSOMock sso = Mockito.mock(SSOMock.class);
-        AuthenticateUser auth = new AuthenticateUser(sso);
-        String username = "ibea707";
         String password = "wrongpassword";
 
         Mockito.when(sso.verify(username, password)).thenReturn("Unsuccessful");
@@ -38,9 +41,6 @@ public class SSOIntegrationTest {
     }
     @Test
     public void testAlreadyLoggedIn(){
-        SSOMock sso = Mockito.mock(SSOMock.class);
-        AuthenticateUser auth = new AuthenticateUser(sso);
-
         Mockito.when(sso.loggedIn()).thenReturn("Logged in");
 
         Boolean login = auth.alreadyLoggedIn();
