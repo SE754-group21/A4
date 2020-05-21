959abda0604cd0a4d83816b65b75e7c9145f2757
lucycy98 <lucy.chen.nz@gmail.com>
2020-05-21 18:05:07 +1200

    implementation: getConcessionFromID

--- src/main/java/ConcessionApplicationHandler.java
+++ src/main/java/ConcessionApplicationHandler.java
@@ -81,7 +81,7 @@ public class ConcessionApplicationHandler {
     }
 
     public ConcessionApplication getConcession(String cid) {
-        return null;
+        return db.getConcessionApplication(cid);
     }
 
 
